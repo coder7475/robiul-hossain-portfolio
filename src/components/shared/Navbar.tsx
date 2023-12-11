de1b50aaaef5cc65406222a7ab5bafcfaf370bef
coder7475 <robiulhossain7475@gmail.com>
2023-12-11 11:48:05 +0600

    fear: change navbar structure

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,43 +1,20 @@
-import Image from "next/image";
-import Link from "next/link";
+import Link from 'next/link';
+
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 z-10 block w-full max-w-full px-4 py-2 text-white bg-white border rounded-none shadow-md h-max border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4" id="home">
-      <div className="flex items-center justify-between text-blue-gray-900">
-        <Link
-          href="#home"
-          className="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased"
-        >
-          Portofolio
-        </Link>
-        <div className="flex items-center gap-4">
-          <div className="hidden mr-4 lg:block">
-            <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-              <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                <Link href="#home" className="flex items-center">
-                  Home
-                </Link>
-              </li>
-              <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                <Link href="#about" className="flex items-center">
-                  About Me
-                </Link>
-              </li>
-              <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                <Link href="#projects" className="flex items-center">
-                  Projects
-                </Link>
-              </li>
-              <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                <Link href="#" className="flex items-center">
-                  Contact Me
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+    <header className="text-gray-600">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <span className="ml-3 text-xl">Portofolio</span>
+        </a>
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <Link className="mr-5 hover:text-gray-900" href="#about">About Me</Link>
+          <Link className="mr-5 hover:text-gray-900" href="#projects">Projects</Link>
+          <Link className="mr-5 hover:text-gray-900" href="#contact">Contact</Link>
+          <Link className="mr-5 hover:text-gray-900" href="#education">Education</Link>
+        </nav>
       </div>
-    </nav>
+    </header>
   );
 };
 
