c1efd59c524fde926ae632f6650dda3f2e21264c
coder7475 <robiulhossain7475@gmail.com>
2023-12-11 11:59:10 +0600

    feat: apply color pallete on navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -2,16 +2,16 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <header className="text-gray-600">
+    <header className="text-light bg-primary ">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <a className="flex title-font font-medium items-center justify-center text-light mb-4 md:mb-0">
           <span className="ml-3 text-xl">Portofolio</span>
         </a>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <Link className="mr-5 hover:text-gray-900" href="#about">About Me</Link>
-          <Link className="mr-5 hover:text-gray-900" href="#projects">Projects</Link>
-          <Link className="mr-5 hover:text-gray-900" href="#contact">Contact</Link>
-          <Link className="mr-5 hover:text-gray-900" href="#education">Education</Link>
+          <Link className="mr-5 hover:text-gray " href="#about">About Me</Link>
+          <Link className="mr-5 hover:text-gray" href="#projects">Projects</Link>
+          <Link className="mr-5 hover:text-gray" href="#contact">Contact</Link>
+          <Link className="mr-5 hover:text-gray" href="#education">Education</Link>
         </nav>
       </div>
     </header>
