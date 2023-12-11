b699aea798665966819ce272c185e60899b054ad
coder7475 <robiulhossain7475@gmail.com>
2023-12-11 12:53:12 +0600

    fix: responsiveness issues with banner

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -4,15 +4,16 @@ import profilePic from './../../assets/profile.png';
 const Banner = () => {
   return (
     <section className="text-light bg-primary">
+      <br />
       <div className="container mx-auto flex px-5 lg:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/4 flex flex-col md:items-start md:text-left text-center">
-          <h1 className="sm:text-4xl lg:text-5xl mb-4 font-bold text-light">
+        <div className="lg:flex-grow w-full md:w-3/4 flex flex-col md:items-start md:text-left text-center">
+          <h1 className="text-4xl lg:text-5xl md:mb-4 font-bold text-light">
            Robiul Hossain
           </h1>
           <p className="mb-8 leading-relaxed font-light">
             Front End Developer
           </p>
-          <p className="mb-8 leading-relaxed max-w-lg">
+          <p className="md:mb-8 leading-relaxed max-w-lg">
             Hello, I am Robiul Hossain, a Front End Developer with a primary focus on crafting exceptional web experiences using Next.js and the MERN (MongoDB, Express.js, React, Node.js) stack.
           </p>
         </div>
