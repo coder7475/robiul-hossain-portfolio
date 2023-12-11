a6248ffb2d5bf4196cbf8b68c0008a3b495a115b
coder7475 <robiulhossain7475@gmail.com>
2023-12-11 12:47:59 +0600

    feat: add content and color to banner

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -4,20 +4,19 @@ import profilePic from './../../assets/profile.png';
 const Banner = () => {
   return (
     <section className="text-light bg-primary">
-      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+      <div className="container mx-auto flex px-5 lg:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/4 flex flex-col md:items-start md:text-left text-center">
+          <h1 className="sm:text-4xl lg:text-5xl mb-4 font-bold text-light">
            Robiul Hossain
           </h1>
-          <p className="mb-8 leading-relaxed">
-            Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-            plant cold-pressed tacos poke beard tote bag. Heirloom echo park
-            mlkshk tote bag selvage hot chicken authentic tumeric truffaut
-            hexagon try-hard chambray.
+          <p className="mb-8 leading-relaxed font-light">
+            Front End Developer
           </p>
-        
+          <p className="mb-8 leading-relaxed max-w-lg">
+            Hello, I am Robiul Hossain, a Front End Developer with a primary focus on crafting exceptional web experiences using Next.js and the MERN (MongoDB, Express.js, React, Node.js) stack.
+          </p>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <div className="">
           <Image
             className="object-cover object-center rounded"
             alt="hero"
