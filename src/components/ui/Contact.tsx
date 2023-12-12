bbc908fb7226ce696fe681578d5ff8b30aaf9731
coder7475 <robiulhossain7475@gmail.com>
2023-12-12 07:13:51 +0600

    feat: add a messanger button

--- src/components/ui/Contact.tsx
+++ src/components/ui/Contact.tsx
@@ -5,15 +5,24 @@ const Contact = () => {
   return (
     <div id="contact" className="min-h-screen bg-secondary py-4">
       <h1 className="text-4xl text-center font-bold text-gray">Contact</h1>
-      <div class="pt-5 text-center text-gray py-5 pt-10">
+      <div class="text-center text-gray py-5 pt-10">
         <p className="text-xl font-semibold">
-          Contact me via:{" "}
+          Message me via:{" "}
           <span className="flex justify-center gap-2 items-center mt-4">
             <Link
-              href="https://wa.me/8801774757227?text=I%20am%20interested%20in%20your%20car%20for%20sale"
+              href="https://wa.me/8801774757227?text=I%20am%20interested%20in%20working%20with%20you%20!"
               className="mb-2 inline-block rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out bg-gray hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
             >
               <Image src="/image/whatUp.png" alt="whatsApp" width="100"  height="100" />
+            </Link>
+            </span>
+          <span className="flex justify-center gap-2 items-center mt-4">
+           
+            <Link
+              href="https://m.me/rhfahadchy/"
+              className="mb-2 inline-block rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out bg-gray hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+            >
+              <Image src="/image/messanger.png" alt="messanger" width={50}  height={50} />
             </Link>
           </span>
         </p>
