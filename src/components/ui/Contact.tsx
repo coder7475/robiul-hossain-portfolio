9b39865a49dfa099caf6002fa9d5e1f193904b38
coder7475 <robiulhossain7475@gmail.com>
2023-12-12 09:56:00 +0600

    fix: button invisible of messenger, whatsupp and google after building

--- src/components/ui/Contact.tsx
+++ src/components/ui/Contact.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import whatup from "/public/image/whatUp.png";
 
 const Contact = () => {
   return (
@@ -11,37 +12,47 @@ const Contact = () => {
           <span className="flex justify-center gap-2 items-center mt-4">
             <Link
               href="https://wa.me/8801774757227?text=I%20am%20interested%20in%20working%20with%20you%20!"
-              className="mb-2 inline-block rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out bg-gray hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+              className="mb-2 inline-block rounded px-6 py-2.5 text-xs font-medium uppercase bg-primary leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
             >
-              <Image
-                src="/image/whatUp.png"
-                alt="whatsApp"
-                width="100"
-                height="100"
-              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 bg-white"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
+              </svg>
             </Link>
-          </span>
-          <span className="flex justify-center gap-2 items-center mt-4">
+
             <Link
               href="https://m.me/rhfahadchy/"
-              className="mb-2 inline-block rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out bg-gray hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+              className="mb-2 inline-block rounded px-6 py-2.5 text-xs font-medium uppercase bg-primary  leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
             >
-              <Image
-                src="/image/messanger.png"
-                alt="messanger"
-                width={50}
-                height={50}
-              />
+              <svg
+                className="h-5 w-5"
+                fill="currentColor"
+                viewbox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                fillRule="evenodd"
+                clipRule="evenodd"
+              >
+                <path d="M12 0c-6.627 0-12 4.975-12 11.111 0 3.497 1.745 6.616 4.472 8.652v4.237l4.086-2.242c1.09.301 2.246.464 3.442.464 6.627 0 12-4.974 12-11.111 0-6.136-5.373-11.111-12-11.111zm1.193 14.963l-3.056-3.259-5.963 3.259 6.559-6.963 3.13 3.259 5.889-3.259-6.559 6.963z" />
+              </svg>
             </Link>
-          </span>
-          <span className="flex justify-center gap-2 items-center mt-4">
-            <Link href="mailto:robiulhossain7475@gmail.com">
-              <Image
-                src="https://img.shields.io/badge/Google-white?style=for-the-badge&logo=google&logoColor=white"
-                alt="Google Badge"
-                width={200}
-                height={100}
-              />
+         
+            <Link href="mailto:robiulhossain7475@gmail.com" className="mb-2 inline-block rounded px-6 py-2.5 text-xs font-medium uppercase bg-primary  leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M7 11v2.4h3.97c-.16 1.029-1.2 3.02-3.97 3.02-2.39 0-4.34-1.979-4.34-4.42 0-2.44 1.95-4.42 4.34-4.42 1.36 0 2.27.58 2.79 1.08l1.9-1.83c-1.22-1.14-2.8-1.83-4.69-1.83-3.87 0-7 3.13-7 7s3.13 7 7 7c4.04 0 6.721-2.84 6.721-6.84 0-.46-.051-.81-.111-1.16h-6.61zm0 0 17 2h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                />
+              </svg>
             </Link>
           </span>
         </div>
