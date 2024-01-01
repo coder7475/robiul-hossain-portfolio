290a1aa9e077e3008e641fcf8ffa485e80c3d377
coder7475 <robiulhossain7475@gmail.com>
2024-01-01 22:22:01 +0600

    feat: improve the technologies section

--- src/components/ui/Technology.tsx
+++ src/components/ui/Technology.tsx
@@ -2,30 +2,23 @@ import Image from 'next/image';
 
 const Technology = () => {
   return (
-    <section className="flex flex-col gap-4 justify-center items-center min-h-screen bg-secondary">
-      <h1 className="text-light font-bold text-center text-4xl mb-10">Technologies </h1>
+    <section className="flex flex-col gap-4 justify-center items-center min-h-screen bg-secondary my-10">
+      <h1 className="font-bold text-center md:text-3xl lg:text-4xl mb-10">My Technologies</h1>
       <p>
+      
         <a href="https://skillicons.dev">
-          <Image src="https://skillicons.dev/icons?i=html,css,javascript,git" width="600" height="500" alt="tech"/>
+          <Image src="https://skillicons.dev/icons?i=html,css,javascript,react,redux,nodejs,expressjs,mongodb,nextjs" width="1000" height="1000" alt="core"/>
         </a>
       </p>
+      <hr/>
+      <h1 className="font-bold text-center md:text-3xl lg:text-4xl my-10">My Tools</h1>
       <p>
         <a href="https://skillicons.dev">
-          <Image src="https://skillicons.dev/icons?i=tailwindcss,materialui,react,github" width="600" height="500" alt="tech"/>
-        </a>
-      </p>
-
-      <p>
-        <a href="https://skillicons.dev">
-          <Image src="https://skillicons.dev/icons?i=firebase,express,mongodb,vite" width="600" height="500" alt="tech"/>
+          <Image src="https://skillicons.dev/icons?i=vscode,vite,git,github,postman" width="500" height="500" alt="tech"/>
         </a>
       </p>
 
-      <p>
-        <a href="https://skillicons.dev">
-          <Image src="https://skillicons.dev/icons?i=nodejs,nextjs" width="300" height="300" alt="tech"/>
-        </a>
-      </p>
+
     </section>
   );
 };
