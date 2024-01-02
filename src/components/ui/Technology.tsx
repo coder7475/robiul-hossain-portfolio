210a5c657e0fc6dd73c3705508ed14aa11e38960
coder7475 <robiulhossain7475@gmail.com>
2024-01-02 20:00:37 +0600

    fix: delete align attritube from technology component

--- src/components/ui/Technology.tsx
+++ src/components/ui/Technology.tsx
@@ -4,7 +4,7 @@ const Technology = () => {
   return (
     <section className="flex flex-col gap-4 justify-start items-center lg:min-h-screen bg-secondary my-10 mx-1">
       <h1 className="font-bold text-center text-2xl md:text-3xl lg:text-4xl pt-4 md:pt-10 md:my-10">My Technologies</h1>
-      <p align="center">      
+      <p>      
         <a href="https://skillicons.dev">
           <Image src="https://skillicons.dev/icons?i=html,css,javascript,react,redux,expressjs,mongodb,nextjs&perline=4" width="500" height="500" alt="core"/>
         </a>
