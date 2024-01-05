2b70f04becba7e380c6ff48c8472f3ae105959e2
coder7475 <robiulhossain7475@gmail.com>
2024-01-05 20:25:07 +0600

    feat: disable clicking technology and tool icons

--- src/components/ui/Technology.tsx
+++ src/components/ui/Technology.tsx
@@ -5,14 +5,14 @@ const Technology = () => {
     <section className="flex flex-col gap-4 justify-start items-center lg:min-h-screen bg-secondary my-10 mx-1">
       <h1 className="font-bold text-center text-2xl md:text-3xl lg:text-4xl pt-4 md:pt-10 md:my-10">My Technologies</h1>
       <p>      
-        <a href="https://skillicons.dev">
+        <a href="https://skillicons.dev" style={{pointerEvents: "none"}}>
           <Image src="https://skillicons.dev/icons?i=html,css,javascript,react,redux,expressjs,mongodb,nextjs&perline=4" width="500" height="500" alt="core"/>
         </a>
       </p>
       <hr/>
       <h1 className="font-bold text-center md:text-3xl lg:text-4xl md:my-10 text-2xl">My Tools</h1>
       <p>
-        <a href="https://skillicons.dev">
+        <a href="https://skillicons.dev" style={{pointerEvents: "none"}}>
           <Image src="https://skillicons.dev/icons?i=vscode,vite,git,github,postman,linux" width="500" height="500" alt="tools"/>
         </a>
       </p>
