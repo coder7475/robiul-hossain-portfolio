1aea6c63f978317b0d142f15ca7d7fa1cca05339
Robiul Hossain <robiulhossain7475@gmail.com>
2024-06-11 07:12:17 +0000

    feat: done updating banner text

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -7,8 +7,8 @@ function MyDescription() {
   return (
     <p className="md:mb-8 leading-relaxed lg:w-2/3">
       I am a software engineer focusing on web technologies. I am skilled in creating client web app using
-      React. I am currently exploring backend development with node.js, typecript. I want to become a principle software engineer in 
-      next 5 years.
+      React. I am currently exploring backend development with Node.js, TypeScript. I want to become a 
+      principle software engineer in next 5 years.
     </p>
   );
 }
@@ -16,11 +16,11 @@ function MyDescription() {
 function LeftBannerText() {
   return (
     <div className="lg:flex-grow w-full md:w-3/4 flex flex-col md:items-start md:text-left text-center">
-      <p className="text-[22px] py-2">Hi, I am</p>
+      <p className="text-[22px] py-2 font-light">Hi, I am</p>
       <h1 className="text-4xl lg:text-5xl md:mb-4 font-bold text-light">
         Robiul Hossain
       </h1>
-      <p className="mb-8 leading-relaxed font-light">Junior Web Developer</p>
+      <p className="mb-8 leading-relaxed">Software Enginner</p>
       <MyDescription></MyDescription>
       <ResumeBtn></ResumeBtn>
     </div>
