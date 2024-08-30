72c027c66bef139e1efbc202980ddc444f81687c
coder7475 <robiulhossain7475@gmail.com>
2024-08-30 23:17:00 +0600

    feat: update technologies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
-import Navbar from '../components/shared/Navbar';
-import Banner from '../components/ui/Banner';
-import Technology from '../components/ui/Technology';
-import Footer from '../components/shared/Footer';
-import Projects from '../components/ui/Projects';
-import Contact from '../components/ui/Contact';
-import Education from '../components/ui/Education';
-import Experience from '../components/ui/Experience';
+import Navbar from "../components/shared/Navbar";
+import Banner from "../components/ui/Banner";
+import Technology from "../components/ui/Technology";
+import Footer from "../components/shared/Footer";
+import Projects from "../components/ui/Projects";
+import Contact from "../components/ui/Contact";
+import Education from "../components/ui/Education";
+// import Experience from "../components/ui/Experience";
 
 const HomePage = () => {
   return (
     <div>
-      <Navbar/>
-      <Banner/>
-      <Technology/>
-      <Projects/>
-      <Contact/>
-      <Education/>
-      {/* <Experience/> */}
-      <Footer/>  
+      <Navbar />
+      <Banner />
+      <Technology />
+      <Projects />
+      <Contact />
+      <Education />
+      {/* <Experience /> */}
+      <Footer />
     </div>
   );
 };
--- src/components/ui/Technology.tsx
+++ src/components/ui/Technology.tsx
@@ -1,22 +1,35 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const Technology = () => {
   return (
     <section className="flex flex-col gap-4 justify-start items-center lg:min-h-screen bg-secondary my-10 mx-1">
-      <h1 className="font-bold text-center text-2xl md:text-3xl lg:text-4xl pt-4 md:pt-10 md:my-10">My Technologies</h1>
-      <p>      
-        <a href="https://skillicons.dev" style={{pointerEvents: "none"}}>
-          <Image src="https://skillicons.dev/icons?i=html,css,javascript,react,redux,expressjs,mongodb,nextjs&perline=4" width="500" height="500" alt="core"/>
-        </a>
-      </p>
-      <hr/>
-      <h1 className="font-bold text-center md:text-3xl lg:text-4xl md:my-10 text-2xl">My Tools</h1>
+      <h1 className="font-bold text-center text-2xl md:text-3xl lg:text-4xl pt-4 md:pt-10 md:my-10">
+        My Technologies
+      </h1>
       <p>
-        <a href="https://skillicons.dev" style={{pointerEvents: "none"}}>
-          <Image src="https://skillicons.dev/icons?i=vscode,vite,git,github,postman,linux" width="500" height="500" alt="tools"/>
+        <a href="https://skillicons.dev" style={{ pointerEvents: "none" }}>
+          <Image
+            src="https://skillicons.dev/icons?i=html,css,javascript,typescript,python,react,redux,expressjs,mongodb,postgres,nextjs,jest,vitest,cypress,prisma&perline=5"
+            width="500"
+            height="500"
+            alt="core"
+          />
         </a>
       </p>
-
+      <hr />
+      <h1 className="font-bold text-center md:text-3xl lg:text-4xl md:my-10 text-2xl">
+        My Tools
+      </h1>
+      <p>
+        <a href="https://skillicons.dev" style={{ pointerEvents: "none" }}>
+          <Image
+            src="https://skillicons.dev/icons?i=vscode,vite,git,github,postman,linux,docker,nginx"
+            width="500"
+            height="500"
+            alt="tools"
+          />
+        </a>
+      </p>
     </section>
   );
 };
