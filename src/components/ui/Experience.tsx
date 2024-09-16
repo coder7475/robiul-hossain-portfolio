07ecb7b1d511f3c33ad24a794d98fe7cc4c3d61a
coder7475 <robiulhossain7475@gmail.com>
2024-09-16 21:37:36 +0600

    feat: add experiences

--- src/components/ui/Experience.tsx
+++ src/components/ui/Experience.tsx
@@ -2,27 +2,24 @@ import React from "react";
 
 const Experience = () => {
   return (
-    <section id="experience" className="py-20">
+    <section id="experience" className="py-20 text-center">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            Experience
+            Experiences
           </h2>
-          <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+          <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4 w-96 sm:w-full">
             Here is a list of my work experience.
           </p>
         </div>
         <div className="mt-10">
-          <h3 className="text-2xl font-bold tracking-tight text-gray-900">
-            Work Experience
-          </h3>
           <div className="mt-5">
             <div className="mb-5">
               <h4 className="text-xl font-bold tracking-tight text-gray-900">
                 DevOps Engineer at OctopusX
               </h4>
               <p className="text-gray-500">August 2024 - Present</p>
-              <p className="text-gray-500">
+              <p className="text-gray-500 mx-auto w-96 sm:w-full">
                 I manage the deployment and monitoring of multiple web
                 applications to ensure their availability and performance,
                 utilizing a CI/CD pipeline with GitHub Actions.
@@ -33,17 +30,17 @@ const Experience = () => {
                 Software Engineer at OctopusX
               </h4>
               <p className="text-gray-500">April 2018 - Present</p>
-              <p className="text-gray-500">
+              <p className="text-gray-500 w-96 sm:w-full">
                 Developing and maintaining multiple web applications using
                 React, Node.js, and MongoDB.
               </p>
             </div>
             <div className="mb-5">
               <h4 className="text-xl font-bold tracking-tight text-gray-900">
-                Intern Monster Studio
+                Web Developer Intern at Monster Studio
               </h4>
               <p className="text-gray-500">February 2024 - March 2024</p>
-              <p className="text-gray-500">
+              <p className="text-gray-500 w-96 sm:w-full">
                 Contributed to the development of a web app using MERN Stack.
               </p>
             </div>
