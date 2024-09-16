afc4aaf9d89a3a7f815334efc6ac94fa8f928a36
coder7475 <robiulhossain7475@gmail.com>
2024-09-16 21:45:38 +0600

    feat: some improvement to navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,15 +1,38 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { AiOutlineMenuFold } from "react-icons/ai";
 import NavgiationLinks from "../ui/NavigationLinks";
 
 const Navbar = () => {
   const [isToggleOpen, setIsToggleOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <header className="border-b-1 sticky top-0 z-20 w-full border-b border-slate-200 bg-slate-100  shadow-lg shadow-slate-700/5 after:absolute after:top-full after:left-0 after:z-10 after:block after:h-px after:w-full after:bg-slate-600 lg:border-slate-600 lg:backdrop-blur-sm lg:after:hidden text-xl px-4">
+    <header
+      className={`border-b-1 sticky top-0 z-20 w-full border-b border-slate-200 ${
+        isScrolled
+          ? "bg-slate-100 shadow-lg shadow-slate-700/5 after:absolute after:top-full after:left-0 after:z-10 after:block after:h-px after:w-full after:bg-slate-600 lg:border-slate-600 lg:backdrop-blur-sm lg:after:hidden text-xl px-4"
+          : "bg-transparent"
+      }`}
+    >
       <div className="relative max-w-7xl mx-auto">
         <nav
           aria-label="main navigation"
