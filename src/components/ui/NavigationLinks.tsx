8579bbddafe697e42edc433ec43c31a543157b5b
coder7475 <robiulhossain7475@gmail.com>
2024-09-16 21:26:01 +0600

    feat: upgrade navbar links ui

--- src/components/ui/NavigationLinks.tsx
+++ src/components/ui/NavigationLinks.tsx
@@ -5,7 +5,7 @@ const NavigationLinks = (props: { isToggleOpen: boolean }) => {
     <ul
       role="menubar"
       aria-label="Select page"
-      className={`absolute top-0 left-0 z-[-1] h-[28.5rem] w-full justify-center overflow-hidden  overflow-y-auto overscroll-contain bg-slate-100 px-8 pb-12 pt-24 font-medium transition-[opacity,visibility] duration-300 lg:visible lg:relative lg:top-0  lg:z-0 lg:flex lg:h-full lg:w-auto lg:items-stretch lg:overflow-visible lg:bg-white/0 lg:px-0 lg:py-0  lg:pt-0 lg:opacity-100 ${
+      className={`absolute top-0 left-0 z-[-1] h-[28.5rem] w-full justify-center overflow-hidden overflow-y-auto overscroll-contain bg-slate-100/90 px-8 pb-12 pt-24 font-medium transition-[opacity,visibility] duration-300 lg:visible lg:relative lg:top-0 lg:z-0 lg:flex lg:h-full lg:w-auto lg:items-stretch lg:overflow-visible lg:bg-white/0 lg:px-0 lg:py-0 lg:pt-0 lg:opacity-100 ${
         props.isToggleOpen
           ? "visible opacity-100 backdrop-blur-sm"
           : "invisible opacity-0"
@@ -30,6 +30,16 @@ const NavigationLinks = (props: { isToggleOpen: boolean }) => {
           href="#education"
         >
           <span>Education</span>
+        </Link>
+      </li>
+      <li role="none" className="flex items-stretch">
+        <Link
+          role="menuitem"
+          aria-haspopup="false"
+          className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600 focus:outline-none focus-visible:outline-none lg:px-8"
+          href="#experience"
+        >
+          <span>Experience</span>
         </Link>
       </li>
       <li role="none" className="flex items-stretch">
