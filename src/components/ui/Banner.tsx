5aff951bfee6cdad823c657cd77f5885b3326622
coder7475 <robiulhossain7475@gmail.com>
2024-09-16 21:48:04 +0600

    feat: improve banner

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -1,41 +1,41 @@
 import Image from "next/image";
 import profilePic from "./../../assets/preview.png";
-// import Link from "next/link";
 import ResumeBtn from "./ResumeBtn";
 
-function MyDescription() {
+const MyDescription = () => {
   return (
-    <p className="md:mb-8 leading-relaxed lg:w-2/3">
+    <p className="md:mb-8 leading-relaxed lg:w-2/3 text-lg font-medium">
       Hello, I am a software engineer focusing on web technologies. I am
       passionate about working across all stages of software development
-      lifecyle. Currently I am focusing on the deployment and maintenance stage.
+      lifecycle. Currently, I am focusing on the deployment and maintenance
+      stage.
     </p>
   );
-}
+};
 
-function LeftBannerText() {
+const LeftBannerText = () => {
   return (
-    <div className="lg:flex-grow w-full md:w-3/4 flex flex-col md:items-start md:text-left text-center">
-      {/* <p className="text-[22px] py-2 font-light">Hi, I am</p> */}
+    <div className="lg:flex-grow w-full md:w-3/4 flex flex-col md:items-start md:text-left text-center p-8">
       <h1 className="text-4xl lg:text-5xl md:mb-4 font-bold text-light">
         Robiul Hossain
       </h1>
-      <p className="mb-8 leading-relaxed">DevOps Engineer</p>
+      <p className="mb-8 leading-relaxed text-lg font-medium">
+        Software Engineer & DevOps Engineer
+      </p>
       <MyDescription />
       <ResumeBtn />
     </div>
   );
-}
+};
 
 const Banner = () => {
   return (
-    <section className="flex justify-center items-center max-w-[80%] mx-auto py-14 text-medium">
-      <br />
-      <div className="container flex justify-center lg:flex-row flex-col items-center">
+    <section className="flex justify-center items-center max-w-[80%] mx-auto py-14 text-medium bg-gray-100 rounded-lg shadow-lg p-4">
+      <div className="container flex justify-center lg:flex-row flex-col items-center p-4">
         <LeftBannerText />
-        <div className="flex items-center">
+        <div className="flex items-center p-4">
           <Image
-            className=""
+            className="rounded-lg"
             alt="hero"
             src={profilePic}
             width={720}
